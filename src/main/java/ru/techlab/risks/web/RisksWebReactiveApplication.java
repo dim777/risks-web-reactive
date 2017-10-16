2e9b36030c50fdbdcfb358137818e9b617e3f2dc
dim777 <dim777@ya.ru>
2017-10-16 08:10:03 +0300

    Tmp

---
+++ src/main/java/ru/techlab/risks/web/RisksWebReactiveApplication.java
@@ -0,0 +1,12 @@
+package ru.techlab.risks.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisksWebReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisksWebReactiveApplication.class, args);
+	}
+}
---
+++ src/main/java/ru/techlab/risks/web/repository/LoanQualityResultRepository.java
@@ -0,0 +1,20 @@
+package ru.techlab.risks.web.repository;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
+import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
+import org.springframework.stereotype.Repository;
+import ru.techlab.risks.rest.risksrestservices.model.LoanQualityResult;
+
+import java.util.stream.Stream;
+
+/**
+ * Created by dim777 on 05.10.17.
+ */
+@Repository
+@Cacheable("loanQualityResultRepository")
+public interface LoanQualityResultRepository extends ReactiveCassandraRepository<{
+    @Query("SELECT * FROM SRRU WHERE RRABD = ?0 AND RRAND = ?1 AND RRASD = ?2")
+    Stream<LoanQualityResult> findSimpleDelayByLoan(String branch, String loanAccountNumber, String loanAccountSuffix);
+}
